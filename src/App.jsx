91d72c5db484a5c5ec02f00c60b021c3dbc31fc2
Deniya-Edwinraj <deniyaedwinraj@gmail.com>
2025-10-08 21:33:26 +0530

    3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/Styles/Style.css";
+import "./assets/styles/Style.css";
 
 // Layout
 import Header from "./Components/Header";
